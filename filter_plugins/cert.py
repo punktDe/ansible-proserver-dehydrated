ec90dfdcfb52f495681ecf9b74c521f86ff50a80
Florian Kaiser <florian.kaiser@fnkr.net>
2020-09-01 14:28:31 +0200

    TASK: Refactor cert filter plugins

--- filter_plugins/cert.py
+++ filter_plugins/cert.py
@@ -1,106 +1,115 @@
 import os
+import unittest
 
 
-def cert_name(dehydrated, domain):
-    if 'domains' not in dehydrated.keys():
-        return None
+class Cert:
+    @staticmethod
+    def name(dehydrated, domain):
+        if 'domains' not in dehydrated.keys():
+            return None
 
-    for crt_name, crt_domains in dehydrated['domains'].items():
-        if domain == crt_name or domain in crt_domains:
-            return crt_name
-
-        for crt_domain in [crt_name] + crt_domains:
-            if crt_domain.startswith('*.') and crt_domain[2:] == domain[domain.find('.') + 1:]:
+        for crt_name, crt_domains in dehydrated['domains'].items():
+            if domain == crt_name or domain in crt_domains:
                 return crt_name
 
-    return None
+            for crt_domain in [crt_name] + crt_domains:
+                if crt_domain.startswith('*.') and crt_domain[2:] == domain[domain.find('.') + 1:]:
+                    return crt_name
 
-
-def cert_exists(dehydrated, domain):
-    return True if cert_name(dehydrated, domain) else False
-
-
-def cert_file(dehydrated, domain, file):
-    name = cert_name(dehydrated, domain)
-    if not name:
         return None
 
-    return os.path.join(dehydrated['prefix']['certs'], name, file)
+    @staticmethod
+    def exists(dehydrated, domain):
+        return True if Cert.name(dehydrated, domain) else False
+
+    @staticmethod
+    def file(dehydrated, domain, file):
+        name = Cert.name(dehydrated, domain)
+        if not name:
+            return None
+
+        return os.path.join(dehydrated['prefix']['certs'], name, file)
+
+    @staticmethod
+    def cert(dehydrated, domain):
+        return Cert.file(dehydrated, domain, 'cert.pem')
+
+    @staticmethod
+    def chain(dehydrated, domain):
+        return Cert.file(dehydrated, domain, 'chain.pem')
+
+    @staticmethod
+    def fullchain(dehydrated, domain):
+        return Cert.file(dehydrated, domain, 'fullchain.pem')
+
+    @staticmethod
+    def privkey(dehydrated, domain):
+        return Cert.file(dehydrated, domain, 'privkey.pem')
 
 
-def cert_cert(dehydrated, domain):
-    return cert_file(dehydrated, domain, 'cert.pem')
+class CertTest(unittest.TestCase):
+    def test_name(self):
+        self.assertEqual(
+            Cert.name({'domains': {
+                'foo.com': ['foo.net', 'foo.org', 'foo.info'],
+                'bar.com': ['bar.net', 'bar.org', 'bar.info'],
+                'baz.com': ['baz.net', 'baz.org', 'baz.info'],
+            }}, 'bar.org'),
+            'bar.com',
+        )
+        self.assertEqual(
+            Cert.name(
+                {'domains': {
+                    'foo.com': ['foo.net', 'foo.org', 'foo.info'],
+                    'bar.com': ['bar.net', 'bar.org', 'bar.info'],
+                    'baz.com': ['baz.net', 'baz.org', 'baz.info'],
+                }}, 'baz.com'),
+            'baz.com',
+        )
+        self.assertEqual(
+            Cert.name({'domains': {
+                '*.*.example.com': ['example.com'],
+                '*.example.com': ['example.com'],
+                'example.net': ['*.example.net'],
+            }}, 'test.example.com'),
+            '*.example.com',
+        )
+        self.assertEqual(
+            Cert.name({'domains': {
+                '*.*.example.com': ['example.com'],
+                '*.example.net': ['example.net'],
+                'example.com': ['*.example.com'],
+            }}, 'test.example.com'),
+            'example.com',
+        )
 
+    def test_exists(self):
+        self.assertEqual(
+            Cert.exists({'domains': {'example.com': ['example.net', 'example.org']}}, 'example.net'),
+            True,
+        )
 
-def cert_chain(dehydrated, domain):
-    return cert_file(dehydrated, domain, 'chain.pem')
-
-
-def cert_fullchain(dehydrated, domain):
-    return cert_file(dehydrated, domain, 'fullchain.pem')
-
-
-def cert_privkey(dehydrated, domain):
-    return cert_file(dehydrated, domain, 'privkey.pem')
-
-
-def cert_name_test():
-    result = cert_name({'domains': {
-        'foo.com': ['foo.net', 'foo.org', 'foo.info'],
-        'bar.com': ['bar.net', 'bar.org', 'bar.info'],
-        'baz.com': ['baz.net', 'baz.org', 'baz.info'],
-    }}, 'bar.org')
-    print(result, result == 'bar.com')
-
-    result = cert_name({'domains': {
-        'foo.com': ['foo.net', 'foo.org', 'foo.info'],
-        'bar.com': ['bar.net', 'bar.org', 'bar.info'],
-        'baz.com': ['baz.net', 'baz.org', 'baz.info'],
-    }}, 'baz.com')
-    print(result, result == 'baz.com')
-
-    result = cert_name({'domains': {
-        '*.*.example.com': ['example.com'],
-        '*.example.com': ['example.com'],
-        'example.net': ['*.example.net'],
-    }}, 'test.example.com')
-    print(result, result == '*.example.com')
-
-    result = cert_name({'domains': {
-        '*.*.example.com': ['example.com'],
-        '*.example.net': ['example.net'],
-        'example.com': ['*.example.com'],
-    }}, 'test.example.com')
-    print(result, result == 'example.com')
-
-
-def cert_exists_test():
-    result = cert_exists(
-        {'domains': {'example.com': ['example.net', 'example.org']}}, 'example.net')
-    print(result, result == True)
-
-
-def cert_fullchain_test():
-    result = cert_fullchain(
-        {'domains': {'example.com': ['example.net', 'example.org']},
-         'prefix': {'certs': '/usr/local/etc/ssl/certs'}}, 'example.net')
-    print(result, result == '/usr/local/etc/ssl/certs/example.com/fullchain.pem')
+    def test_fullchain(self):
+        self.assertEqual(
+            Cert.fullchain(
+                {'domains': {'example.com': ['example.net', 'example.org']},
+                 'prefix': {'certs': '/usr/local/etc/ssl/certs'}}, 'example.net'),
+            '/usr/local/etc/ssl/certs/example.com/fullchain.pem',
+        )
 
 
 class FilterModule(object):
     def filters(self):
         return {
-            'cert_name': cert_name,
-            'cert_exists': cert_exists,
-            'cert_file': cert_file,
-            'cert_cert': cert_cert,
-            'cert_chain': cert_chain,
-            'cert_fullchain': cert_fullchain,
-            'cert_privkey': cert_privkey,
+            'cert_name': Cert.name,
+            'cert_exists': Cert.exists,
+            'cert_file': Cert.file,
+            'cert_cert': Cert.cert,
+            'cert_chain': Cert.chain,
+            'cert_fullchain': Cert.fullchain,
+            'cert_privkey': Cert.privkey,
         }
 
 
 if __name__ == '__main__':
-    cert_name_test()
-    cert_exists_test()
-    cert_fullchain_test()
+    unittest.main()
